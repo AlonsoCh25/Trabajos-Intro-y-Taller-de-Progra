9ccd56899876907782912c81c30c6765c7ab0034
Kenneth <kennethalonsoc@gmail.com>
2019-03-03 18:13:09 -0600

    Update Tarea, suma de binarios KENNETH

--- Tarea, suma de binarios KENNETH.py
+++ Tarea, suma de binarios KENNETH.py
@@ -1,18 +1,25 @@
 #Entrada: Dos numeros binarios
 #Salida: La suma de los dos numeros bianrios ingresados
-#Restricciones: Deben ser numeros binarios de misma longitud (el que meta algo diferente está bateando xD).
+#Restricciones: Deben ser numeros binarios del mismo tamaño (el que meta algo diferente está bateando xD).
 
-def sumabin(bin1, bin2):
+def sumarBinarios(numero1, numero2):
 
-    if isinstance(bin1, int) and isinstance(bin2, int) and (len(str(bin1)) == len(str(bin2))):
-        return int(convertir(sumarAux(bin1, bin2, 0)))
+    if isinstance(numero1, int) and isinstance(numero2, int) and (len(str(numero1)) == len(str(numero2))):
+        return int(convertir(sumarAux(numero1, numero2, 0)))
     else:
         print("Ingresar una entrada válida")
 
-def sumarAux(bin1, bin2, exp):
+def sumarAux(numero1, numero2, exp):
 
-    if bin1 == 0:
+    if numero1 == 0:
         return 0
     else:
-        return (bin1%10 + bin2%10)*(2**exp) + sumarAux(bin1//10, bin2//10, exp+1)
+        return (numero1%10 + numero2%10)*(2**exp) + sumarAux(numero1//10, numero2//10, exp+1)
+
+def convertir(a):
+
+    if a == 0:
+        return ""
+    else:
+        return convertir(a//2) + str(a % 2)
 
