940178e4589590ca635bd27a7ae091df7b5cbc23
Oscarmendez825 <48070861+Oscarmendez825@users.noreply.github.com>
2019-03-19 22:38:48 -0600

    TAREA TALLERX2

--- Tarea, suma de binarios KENNETH.py
+++ Tarea, suma de binarios KENNETH.py
@@ -4,7 +4,7 @@
 
 def sumarBinarios(numero1, numero2):
 
-    if isinstance(numero1, int) and isinstance(numero2, int) and (len(str(numero1)) == len(str(numero2))):
+    if isinstance(numero1, list) and isinstance(numero2, list) and (len(str(numero1)) == len(str(numero2))):
         return int(convertir(sumarAux(numero1, numero2, 0)))
     else:
         print("Ingresar una entrada válida")
@@ -14,12 +14,12 @@ def sumarAux(numero1, numero2, exp):
     if numero1 == 0:
         return 0
     else:
-        return (numero1%10 + numero2%10)*(2**exp) + sumarAux(numero1//10, numero2//10, exp+1)
+        return (numero1[:-1] + numero2[:-1])*(2**exp) + sumarAux(numero1//10, numero2//10, exp+1)
 
 def convertir(a):
 
     if a == 0:
         return ""
     else:
-        return convertir(a//2) + str(a % 2)
+        return convertir(a//2) + str(a [:2])
 
